6582469ebeb42ded87e42baf953d32f123f76456
Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
2024-04-30 10:28:33 -0700

    drm/i915/bmg: Define IS_BATTLEMAGE macro
    BugLink: https://bugs.launchpad.net/bugs/2134640
    
    Display code uses IS_BATTLEMAGE macro but the platform support doesn't
    exist in i915. So fake IS_BATTLEMAGE macro defined to enable building
    i915 code.  We should make sure the macro parameter is used in the
    always-false expression so that we don't run into "unused variable"
    warnings from i915 builds if the IS_BATTLEMAGE() check is the only place
    the i915 pointer gets used in a function.
    
    While we're at it, also update the IS_LUNARLAKE macro to include the
    parameter in the false expression for consistency.
    
    Signed-off-by: Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240430172850.1881525-3-radhakrishna.sripada@intel.com
    (cherry picked from commit 37153b0ad3d3c26fd4b6091b124245a4401da6fd)
    [diewald: prerequisite for 9cb15478916e ("drm/i915/dp_mst: Work around
    Thunderbolt sink disconnect after SINK_COUNT_ESI read")]
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -556,7 +556,15 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
 #define IS_DG2(i915)	IS_PLATFORM(i915, INTEL_DG2)
 #define IS_PONTEVECCHIO(i915) IS_PLATFORM(i915, INTEL_PONTEVECCHIO)
 #define IS_METEORLAKE(i915) IS_PLATFORM(i915, INTEL_METEORLAKE)
-#define IS_LUNARLAKE(i915) 0
+/*
+ * Display code shared by i915 and Xe relies on macros like IS_LUNARLAKE,
+ * so we need to define these even on platforms that the i915 base driver
+ * doesn't support.  Ensure the parameter is used in the definition to
+ * avoid 'unused variable' warnings when compiling the shared display code
+ * for i915.
+ */
+#define IS_LUNARLAKE(i915) (0 && i915)
+#define IS_BATTLEMAGE(i915)  (0 && i915)
 
 #define IS_DG2_G10(i915) \
 	IS_SUBPLATFORM(i915, INTEL_DG2, INTEL_SUBPLATFORM_G10)
