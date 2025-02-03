da99658a5972f707aa1ccf3c922898388cf8dfbe
Ping-Ke Shih <pkshih@realtek.com>
2025-02-03 15:29:08 +0800

    wifi: rtw89: call power_on ahead before selecting firmware
    BugLink: https://bugs.launchpad.net/bugs/2133301
    
    [ Upstream commit d078f5857a00c06fa0ddee26d3cb722e938e1688 ]
    
    Driver selects firmware by hardware version, which normally can be read
    from registers before selecting firmware. However, certain chips such as
    RTL8851B, it needs to read hardware version from efuse while doing
    power_on, but do power_on after selecting firmware in current flow.
    
    To resolve this flow problem, move power_on out from
    rtw89_mac_partial_init(), and call rtw89_mac_pwr_on() separately at
    proper places to have expected flow.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20250203072911.47313-2-pkshih@realtek.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/net/wireless/realtek/rtw89/mac.c
+++ drivers/net/wireless/realtek/rtw89/mac.c
@@ -1490,6 +1490,21 @@ static int rtw89_mac_power_switch(struct rtw89_dev *rtwdev, bool on)
 #undef PWR_ACT
 }
 
+int rtw89_mac_pwr_on(struct rtw89_dev *rtwdev)
+{
+	int ret;
+
+	ret = rtw89_mac_power_switch(rtwdev, true);
+	if (ret) {
+		rtw89_mac_power_switch(rtwdev, false);
+		ret = rtw89_mac_power_switch(rtwdev, true);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 void rtw89_mac_pwr_off(struct rtw89_dev *rtwdev)
 {
 	rtw89_mac_power_switch(rtwdev, false);
@@ -3863,14 +3878,6 @@ int rtw89_mac_partial_init(struct rtw89_dev *rtwdev, bool include_bb)
 {
 	int ret;
 
-	ret = rtw89_mac_power_switch(rtwdev, true);
-	if (ret) {
-		rtw89_mac_power_switch(rtwdev, false);
-		ret = rtw89_mac_power_switch(rtwdev, true);
-		if (ret)
-			return ret;
-	}
-
 	rtw89_mac_ctrl_hci_dma_trx(rtwdev, true);
 
 	if (include_bb) {
@@ -3903,6 +3910,10 @@ int rtw89_mac_init(struct rtw89_dev *rtwdev)
 	bool include_bb = !!chip->bbmcu_nr;
 	int ret;
 
+	ret = rtw89_mac_pwr_on(rtwdev);
+	if (ret)
+		return ret;
+
 	ret = rtw89_mac_partial_init(rtwdev, include_bb);
 	if (ret)
 		goto fail;
@@ -3930,7 +3941,7 @@ int rtw89_mac_init(struct rtw89_dev *rtwdev)
 
 	return ret;
 fail:
-	rtw89_mac_power_switch(rtwdev, false);
+	rtw89_mac_pwr_off(rtwdev);
 
 	return ret;
 }
