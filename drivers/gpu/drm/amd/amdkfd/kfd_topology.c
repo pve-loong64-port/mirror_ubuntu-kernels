44c6b433cfe6cf5fdd49472652b4f1dbf4bf183b
Jonathan Kim <jonathan.kim@amd.com>
2025-03-14 11:08:21 -0400

    drm/amdkfd: set precise mem ops caps to disabled for gfx 11 and 12
    BugLink: https://bugs.launchpad.net/bugs/2133301
    
    [ Upstream commit f82d27dcff939d3cbecbc60e1b71e2518c37e81d ]
    
    Clause instructions with precise memory enabled currently hang the
    shader so set capabilities flag to disabled since it's unsafe to use
    for debugging.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Tested-by: Lancelot Six <lancelot.six@amd.com>
    Reviewed-by: Harish Kasiviswanathan <harish.kasiviswanathan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.c
@@ -1919,10 +1919,6 @@ static void kfd_topology_set_capabilities(struct kfd_topology_device *dev)
 	} else {
 		dev->node_props.debug_prop |= HSA_DBG_WATCH_ADDR_MASK_LO_BIT_GFX10 |
 					HSA_DBG_WATCH_ADDR_MASK_HI_BIT;
-
-		if (KFD_GC_VERSION(dev->gpu) >= IP_VERSION(11, 0, 0))
-			dev->node_props.capability |=
-				HSA_CAP_TRAP_DEBUG_PRECISE_MEMORY_OPERATIONS_SUPPORTED;
 	}
 
 	kfd_topology_set_dbg_firmware_support(dev);
